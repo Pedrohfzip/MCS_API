950ffb585064fee692a99f8566f211357e9841e8
Pedro <pedrohfagundes2002@gmail.com>
2025-11-16 19:16:05 -0300

    feat(models): create User model with UUID, email, password, and role fields

---
+++ api/database/models/user.js
@@ -0,0 +1,29 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../index.js';
+
+const User = sequelize.define('User', {
+  uuid: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'users',
+  timestamps: true,
+});
+
+export default User;
