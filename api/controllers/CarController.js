4e78acefd10ee19b12a87ea050d4b273a9731e75
Pedro <pedrohfagundes2002@gmail.com>
2026-01-14 15:33:16 -0300

    refactor(cars): comment out validation and creation logic in create function

--- api/controllers/CarController.js
+++ api/controllers/CarController.js
@@ -5,19 +5,20 @@ const CarController = {
   // Criar um novo carro
   async create(req, res) {
     const { name, brand, year, photo, gas, color, km } = req.body;
-    if (!name || !brand || !year || !gas || !color || km === undefined) {
-      return res.status(400).json({ erro: 'Name, brand, year, gas, color e km são obrigatórios.' });
-    }
-    try {
-      const novoCarro = await Car.create({ name, brand, year, gas, color, km });
-      if (!novoCarro) {
-        return res.status(500).json({ erro: 'Erro ao cadastrar carro.' });
-      }
-      return res.status(200).json({ mensagem: 'Carro cadastrado com sucesso', carro: novoCarro });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ erro: error.message || 'Erro ao cadastrar carro.' });
-    }
+    console.log(req.body);
+    // if (!name || !brand || !year || !gas || !color || km === undefined) {
+    //   return res.status(400).json({ erro: 'Name, brand, year, gas, color e km são obrigatórios.' });
+    // }
+    // try {
+    //   const novoCarro = await Car.create({ name, brand, year, gas, color, km });
+    //   if (!novoCarro) {
+    //     return res.status(500).json({ erro: 'Erro ao cadastrar carro.' });
+    //   }
+    //   return res.status(200).json({ mensagem: 'Carro cadastrado com sucesso', carro: novoCarro });
+    // } catch (error) {
+    //   console.log(error);
+    //   return res.status(500).json({ erro: error.message || 'Erro ao cadastrar carro.' });
+    // }
   },
   // Listar todos os carros
   async getAllCars(req, res) {
