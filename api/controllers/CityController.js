6df6a39703ae650565fb4c4645dc65e24bdd6f83
Pedro <pedrohfagundes2002@gmail.com>
2026-02-09 22:42:19 -0300

    refactor: update getAllCitys method to fetch cities from database and change route to /getCities

--- api/controllers/CityController.js
+++ api/controllers/CityController.js
@@ -3,21 +3,8 @@ import Cities from '../database/models/City.js';
 const CityController = {
     async getAllCitys(req, res) {
         try {
-            const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/municipios?orderBy=nome');
-            if (!response.ok) {
-                return res.status(500).json({ error: 'Erro ao buscar dados do IBGE' });
-            }
-            const cities = await response.json();
-            cities.forEach(async (city) => {
-                await Cities.create({
-                    city: city.nome,
-                    state: city['regiao-imediata']['regiao-intermediaria']['UF']['nome']
-                });
-            });
-            // cities.forEach(async (city) => {
-            //     console.log(city.nome, city['regiao-imediata']['regiao-intermediaria']['UF']['nome'])
-            //     console.log(city)
-            // });
+            const cities = await Cities.findAll();
+            console.log('Cidades encontradas:', cities);
             return res.status(200).json(cities);
         } catch (error) {
             return res.status(500).json({ error: 'Erro ao buscar dados do IBGE', details: error.message });
--- api/routes/CitysRouter.js
+++ api/routes/CitysRouter.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import CityController from "../controllers/CityController.js";
 const router = Router();
 
-router.get('/allCitys', CityController.getAllCitys)
+router.get('/getCities', CityController.getAllCitys)
 
 export default router;
