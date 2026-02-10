96621030d039c028ba743dcc5caaea4f0edee5db
Pedro <pedrohfagundes2002@gmail.com>
2026-02-09 21:03:16 -0300

    feat: add City model and migration for Cities table

---
+++ api/database/models/City.js
@@ -0,0 +1,34 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../index.js';
+
+const City = sequelize.define('City', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+}, {
+  tableName: 'Cities',
+  timestamps: true,
+});
+
+export default City;
